5772ea698d1b278c5116c2e44f49862afd7e74d5
Bryan Musso <goofybryan@gmail.com>
2021-03-03 21:19:02 +0200

    Some clean up

---
+++ Dependency/src/ZarDevs.DependencyInjection/DependencyInfoExtentions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ZarDevs.DependencyInjection
+{
+    /// <summary>
+    /// Dependency info extensions
+    /// </summary>
+    public static class DependencyInfoExtentions
+    {
+        /// <summary>
+        /// Convert a generic type info into a concrete type info
+        /// </summary>
+        /// <param name="info">The current dependency info</param>
+        /// <param name="concreteRequestType">The concrete type that has been requested.</param>
+        /// <returns>A new instance of the dependency info with the concrete information set.</returns>
+        public static IDependencyInfo As(this IDependencyInfo info, Type concreteRequestType)
+        {
+            if (info is IDependencyTypeInfo typeInfo)
+                return As(typeInfo, concreteRequestType);
+
+            throw new NotSupportedException($"The dependency info {info} is not currently supported.");
+        }
+
+        /// <summary>
+        /// Convert a generic type info into a concrete type info
+        /// </summary>
+        /// <param name="typeInfo">The current dependency info</param>
+        /// <param name="concreteRequestType">The concrete type that has been requested.</param>
+        /// <returns>A new instance of the dependency info with the concrete information set.</returns>
+        public static IDependencyTypeInfo As(this IDependencyTypeInfo typeInfo, Type concreteRequestType)
+        {
+            if(!typeInfo.ResolvedType.IsGenericType)
+                throw new InvalidOperationException($"The info resolved type for '{typeInfo}' is not a generic type.");
+
+            var resovleType = typeInfo.ResolvedType;
+            var concreteResolveType = resovleType.MakeGenericType(concreteRequestType.GenericTypeArguments);
+
+            return new DependencyTypeInfo(concreteResolveType, typeInfo) { RequestType = concreteRequestType };
+        }
+    }
+}
